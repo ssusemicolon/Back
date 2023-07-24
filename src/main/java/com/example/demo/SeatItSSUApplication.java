0043ad018cfbec4e3d6d5e4172da763f8d1061c0
bflykky <kkyxw@naver.com>
2023-07-25 00:52:35 +0900

    프로젝트 이름 변경

--- src/main/java/com/example/demo/SeatItSSUApplication.java
+++ src/main/java/com/example/demo/SeatItSSUApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SeatItSSUApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SeatItSSUApplication.class, args);
 	}
 
 }
